57d59fb117e23ee92120c885092eebdd53e5c7e9
LBHSPreston <selwyn.preston@hackney.gov.uk>
2020-05-19 17:21:51 +0100

    Trimming postcode parameter to ensure no search by whitespaces.

--- cv-19-i-need-help/Gateways/V1/INeedHelpGateway.cs
+++ cv-19-i-need-help/Gateways/V1/INeedHelpGateway.cs
@@ -29,7 +29,7 @@ namespace CV19INeedHelp.Gateways.V1
                 response = _dbContext.ResidentSupportAnnex
                     .Where(x => x.Uprn == uprn).ToList();   
             }
-            else if(!string.IsNullOrEmpty(postcode))
+            else if(!string.IsNullOrEmpty(postcode.Trim()))
             {   
                 response = _dbContext.ResidentSupportAnnex
                     .Where(x => x.Postcode.Contains(postcode)).ToList();   
