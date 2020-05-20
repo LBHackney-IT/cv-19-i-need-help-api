e70e9a691a78e1e376b68b4e8fa3ceb6ebe680a7
LBHSPreston <selwyn.preston@hackney.gov.uk>
2020-05-20 17:05:01 +0100

    Returning all records in a way that can be filtered for master.

--- cv-19-i-need-help/Gateways/V1/INeedHelpGateway.cs
+++ cv-19-i-need-help/Gateways/V1/INeedHelpGateway.cs
@@ -39,7 +39,7 @@ namespace CV19INeedHelp.Gateways.V1
             if (isMaster == true)
             {
                 response = response
-                    .Where(x =>true).ToList();
+                    .Where(x => x.RecordStatus == "MASTER").ToList();
             }
             return response;
         }
