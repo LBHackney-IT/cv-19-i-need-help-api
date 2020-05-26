8bea2bfa68ff3ea772374e4a51e5e9e9aed188e1
LBHSPreston <selwyn.preston@hackney.gov.uk>
2020-05-26 14:26:10 +0100

    Added a test suite for the delivery schedule use case.

---
+++ cv-19-i-need-help-test/UseCases/V1/DeliveryScheduleUseCaseTest.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using CV19INeedHelp.UseCases.V1;
+using CV19INeedHelp.Gateways.V1;
+using CV19INeedHelp.Models.V1;
+using CV19INeedHelp.UseCases.V1;
+using FluentAssertions;
+using NUnit.Framework;
+using Moq;
+
+namespace CV19INeedHelpTest.UseCases.V1
+{
+    [TestFixture]
+    public class DeliveryScheduleUseCaseTest
+    {
+        private readonly IDeliveryScheduleUseCase _classUnderTest;
+        private readonly Mock<IINeedHelpGateway> _fakeGateway;
+
+        public DeliveryScheduleUseCaseTest()
+        {
+            _fakeGateway = new Mock<IINeedHelpGateway>();
+            _classUnderTest = new DeliveryScheduleUseCase(_fakeGateway.Object);
+        }
+        
+        [TestCase]
+        public void CanCallTheDatabaseCreateDeliveryScheduleMethodIfConfirmed()
+        {
+            var limit = 10;
+            var confirmed = true;
+            _classUnderTest.CreateDeliverySchedule(limit, confirmed);
+            _fakeGateway.Verify(m => m.CreateDeliverySchedule(limit), Times.Once);
+        }
+        
+        public void CanCallTheDatabaseCreateTemporaryDeliveryDataMethodIfNotConfirmed()
+        {
+            var limit = 10;
+            var confirmed = false;
+            _classUnderTest.CreateDeliverySchedule(limit, confirmed);
+            _fakeGateway.Verify(m => m.CreateTemporaryDeliveryData(limit), Times.Once);
+        }
+
+
+        // [TestCase("123", "test", false)]
+        // public void GetsAListOfHelpRequestsIfValidUprnIsProvided(string uprn, string postcode, bool isMaster)
+        // {
+        //     _fakeGateway.Setup(x => x.GetHelpRequestsForUprn(uprn, postcode, isMaster)).Returns(response_data);
+        //     var response = _classUnderTest.GetHelpRequests(uprn, postcode, isMaster);
+        //     Assert.AreEqual(response, response_data);
+        //     response.Should().Equal(response_data);
+        // }
+
+        
+        
+    }
+}
