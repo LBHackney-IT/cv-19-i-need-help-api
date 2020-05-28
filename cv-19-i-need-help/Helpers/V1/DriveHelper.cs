443b31cacf5250100a1b8df420c3669ff486462e
LBHSPreston <selwyn.preston@hackney.gov.uk>
2020-05-28 17:25:42 +0100

    Added a utility class to manage Google Drive interactions.

---
+++ cv-19-i-need-help/Helpers/V1/DriveHelper.cs
@@ -0,0 +1,105 @@
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Google.Apis.Services;
+using Google.Apis.Drive.v3;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using CV19INeedHelp.Models.V1;
+using NUnit.Framework.Constraints;
+
+namespace CV19INeedHelp.Helpers.V1
+{
+    public class DriveHelper : IDriveHelper
+    {
+        private readonly string _applicationName;
+        private readonly string _uploadFolder;
+        private readonly UserCredential _credential;
+        private static string[] Scopes = { SheetsService.Scope.Drive, SheetsService.Scope.Spreadsheets, SheetsService.Scope.DriveFile };
+
+        public DriveHelper(string applicationName, string uploadFolder)
+        {
+            _applicationName = applicationName;
+            _uploadFolder = uploadFolder;
+            using (var stream =
+                new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                // The file token.json stores the user's access and refresh tokens, and is created
+                // automatically when the authorization flow completes for the first time.
+                string credPath = "token.json";
+                _credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.Load(stream).Secrets,
+                    Scopes,
+                    "user",
+                    CancellationToken.None
+                    //new FileDataStore(credPath, true)
+                ).Result;
+                Console.WriteLine("Credential file saved to: " + credPath);
+            }
+        }
+
+        public string CreateSpreadsheet(string name)
+        {
+            // Create Google Drive API service.
+            var driveService = new DriveService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = _credential,
+                ApplicationName = _applicationName,
+            });
+
+            var sheetTemplate = new Google.Apis.Drive.v3.Data.File()    
+            {    
+                Name = name,    
+                MimeType = "application/vnd.google-apps.spreadsheet",    
+                Parents = new List<string>    
+                {    
+                    _uploadFolder
+                }    
+            };
+            FilesResource.CreateRequest request;
+            request = driveService.Files.Create(sheetTemplate);    
+            request.Fields = "id";
+            request.SupportsAllDrives = true;
+            var file = request.Execute();
+            return file.Id;
+        }
+
+        public void PopulateSpreadsheet(string sheetId, List<ResidentSupportAnnex> data)
+        {
+            // Create Google Sheets API service.
+            var sheetsService = new SheetsService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = _credential,
+                ApplicationName = _applicationName
+            });
+            var valueRange = new ValueRange();
+            var range = $"Sheet1!A:K";
+            valueRange.Values = new List<IList<object>>( );
+            valueRange.Values.Add(new List<object>{ "Id", "Number Of Packages", "Full Name", "Telephone Number", "Mobile Number", "Address", "Postcode", "UPRN", "Dietary Requirements", "Delivery Notes", "Next Delivery Date"});
+            foreach (var item in data)
+            {
+                    valueRange.Values.Add( new List<object>
+                        {
+                            item.Id,
+                            1,
+                            $"{item.FirstName} {item.LastName}",
+                            item.ContactTelephoneNumber,
+                            item.ContactMobileNumber,
+                            $"{item.AddressFirstLine} {item.AddressSecondLine} {item.AddressThirdLine}",
+                            item.Postcode,
+                            item.Uprn,
+                            item.AnyFoodHouseholdCannotEat,
+                            item.DeliveryNotes,
+                            DateTime.Now.AddDays(1)
+                        }
+                    );
+            }
+            var appendRequest = sheetsService.Spreadsheets.Values.Append(valueRange, sheetId, range);
+            appendRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+            var appendReponse = appendRequest.Execute();
+        }
+    }
+}
