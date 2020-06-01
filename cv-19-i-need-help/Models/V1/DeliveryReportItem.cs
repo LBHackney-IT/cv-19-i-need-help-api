a847583daf66931040b931417a3403daf5ab3c32
LBHSPreston <selwyn.preston@hackney.gov.uk>
2020-06-01 13:41:49 +0100

    A new data type for delivery report records.

---
+++ cv-19-i-need-help/Models/V1/DeliveryReportItem.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace CV19INeedHelp.Models.V1
+{
+    public class DeliveryReportItem
+    {
+        public int Id { get; set; }
+        public int AnnexId { get; set; }
+        public int NumberOfPackages { get; set; }
+        public string FullName { get; set; }
+        public string TelephoneNumber { get; set; }
+        public string MobileNumber { get; set; }
+        public string FullAddress { get; set; }
+        public string Postcode { get; set; }
+        public string Uprn { get; set; }
+        public string AnyFoodHouseholdCannotEat { get; set; }
+        public string DeliveryNotes { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public DateTime LastConfirmedDeliveryDate { get; set; }
+        public int BatchId { get; set; }
+
+    }
+}
