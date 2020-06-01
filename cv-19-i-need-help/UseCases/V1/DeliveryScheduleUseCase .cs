781056f1c9ba00daedd01c0ab0ed9c2e041d47bd
LBHSPreston <selwyn.preston@hackney.gov.uk>
2020-06-01 14:15:56 +0100

    You now have to pass the spreadsheet name to add to the batch information.

--- cv-19-i-need-help/UseCases/V1/DeliveryScheduleUseCase .cs
+++ cv-19-i-need-help/UseCases/V1/DeliveryScheduleUseCase .cs
@@ -26,7 +26,7 @@ namespace CV19INeedHelp.UseCases.V1
                 _driveHelper = new DriveHelper("I Need Help API", "1KpsBDkH3A7ugMPZSb9SCHBeiM7evCqQg");
                 var spreadsheet =
                     _driveHelper.CreateSpreadsheet($"Delivery Report - {helper.GetNextWorkingDay():dd-MM-yyyy}");
-                var data = _iFoodDeliveriesGateway.CreateDeliverySchedule(limit);
+                var data = _iFoodDeliveriesGateway.CreateDeliverySchedule(limit, spreadsheet);
                 _driveHelper.PopulateSpreadsheet(spreadsheet, data);
                 return "https://docs.google.com/spreadsheets/d/" + spreadsheet;
                 
