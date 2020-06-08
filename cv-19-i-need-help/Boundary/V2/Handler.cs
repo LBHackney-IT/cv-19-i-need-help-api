8bddb5ca8abec8dd053afbb878ee02e1e9d8d55f
elcorbs <emma@madetech.com>
2020-06-08 16:32:00 +0100

    refactor v2 handler

--- cv-19-i-need-help/Boundary/V2/Handler.cs
+++ cv-19-i-need-help/Boundary/V2/Handler.cs
@@ -25,56 +25,32 @@ namespace CV19INeedHelp.Boundary.V2
            var getRequestGateway = new INeedHelpGateway(new Cv19SupportDbContext(_connectionString));
            var getRequestsUseCase = new GetHelpRequestsUseCase(getRequestGateway);
            var requestParams = request.QueryStringParameters;
-           bool master = false;
-           string uprn = null;
-           string postcode = null;
+           var master = QueryValueGiven("master", request) && bool.TryParse(requestParams["master"], out var v)
+               ? SetValue(v, "master")
+               : ValueNotSet<bool>("master");
+
+           var uprn = QueryValueGiven("uprn", request)
+               ? SetValue(requestParams["uprn"], "uprn")
+               : ValueNotSet<string>("uprn");
+
+           var postcode = QueryValueGiven("postcode", request)
+               ? SetValue(requestParams["postcode"], "postcode")
+               : ValueNotSet<string>("postcode");
+
            try
            {
-               master = bool.Parse(requestParams["master"]);
-               LambdaLogger.Log("master: " + master);
-           }
-           catch (Exception e)
-           {
-               LambdaLogger.Log("master parameter not provided.");
-           }
-           try
-           {
-               uprn = requestParams["uprn"];
-               LambdaLogger.Log("uprn: " + uprn);
-           }
-           catch (Exception e)
-           {
-               LambdaLogger.Log("uprn parameter not provided.");
-           }
-           try
-           {
-               postcode = requestParams["postcode"];
-               LambdaLogger.Log("postcode: " + postcode);
-           }
-           catch (Exception e)
-           {
-               LambdaLogger.Log("postcode parameter not provided.");
-           }
-           try
-           {
-               var resp = getRequestsUseCase.GetHelpRequests(uprn, postcode, master)
-                   .Select(x => x.ToResponse());
-               LambdaLogger.Log("Records retrieval success: " + JsonConvert.SerializeObject(resp));
+               var resp = ConvertToCamelCasedJson(getRequestsUseCase.GetHelpRequests(uprn, postcode, master).ToResponse());
+               LambdaLogger.Log("Records retrieval success: " + resp);
                return new APIGatewayProxyResponse
                {
-                   IsBase64Encoded = true, StatusCode = 200, Body = ConvertToCamelCasedJson(resp)
+                   IsBase64Encoded = true,
+                   StatusCode = 200,
+                   Body = resp
                };
            }
-           catch(Exception e)
+           catch (Exception e)
            {
-               LambdaLogger.Log("Error: " + e.Message);
-               var response = new APIGatewayProxyResponse
-               {
-                   IsBase64Encoded = true,
-                   StatusCode = 500,
-                   Body = "Error processing request: " + ". Error Details: " + e.Message + e.StackTrace
-               };
-               return response;
+               return SendErrorResponse(e);
            }
        }
 
@@ -97,12 +73,42 @@ namespace CV19INeedHelp.Boundary.V2
            catch(Exception e)
            {
                LambdaLogger.Log("Error: " + e.Message);
-               var response = new Response();
-               response.isBase64Encoded = true;
-               response.statusCode = "500";
-               response.body = "Error processing request: " + ". Error Details: " + e.Message + e.StackTrace;
+               var response = new Response
+               {
+                   isBase64Encoded = true,
+                   statusCode = "500",
+                   body = "Error processing request: " + ". Error Details: " + e.Message + e.StackTrace
+               };
                return response;
            }
+       }
+
+       private static APIGatewayProxyResponse SendErrorResponse(Exception e)
+       {
+           LambdaLogger.Log("Error: " + e.Message);
+           return new APIGatewayProxyResponse
+           {
+               IsBase64Encoded = true,
+               StatusCode = 500,
+               Body = "Error processing request: " + ". Error Details: " + e.Message + e.StackTrace
+           };
+       }
+
+       private static bool QueryValueGiven(string keyName, APIGatewayProxyRequest request)
+       {
+           return request.QueryStringParameters != null && request.QueryStringParameters.ContainsKey(keyName);
+       }
+
+       private static T ValueNotSet<T>(string name)
+       {
+           LambdaLogger.Log($"{name} parameter not provided.");
+           return default;
+       }
+
+       private static T SetValue<T>(T result, string name)
+       {
+           LambdaLogger.Log($"{name}: " + result);
+           return result;
        }
 
        private static string ConvertToCamelCasedJson<T>(T responseBody)
