2b3c6598e679124cb6770a36dc53c003f5753aa4
LBHSPreston <selwyn.preston@hackney.gov.uk>
2020-06-12 16:07:15 +0100

    Added gateway test.

--- cv-19-i-need-help-test/Gateways/V1/INeedHelpGatewayTest.cs
+++ cv-19-i-need-help-test/Gateways/V1/INeedHelpGatewayTest.cs
@@ -1,8 +1,11 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using AutoFixture;
 using CV19INeedHelp.Data.V1;
 using CV19INeedHelp.Gateways.V1;
 using CV19INeedHelp.Models.V1;
+using FluentAssertions;
 using Moq;
 using NUnit.Framework;
 
@@ -32,17 +35,48 @@ namespace CV19INeedHelpTest.Gateways.V1
         {
             Environment.SetEnvironmentVariable("CV_19_DB_CONNECTION", _connectionString);
             ClearResidentSupportAnnexTable();
+            ClearDeliveryBatchTable();
+            ClearDeliveryReportDataTable();
         }
 
-        public void WhenCreatingADeliveryBatchReturnAValidObject()
+        [Test]
+        public void WhenCreatingADeliveryBatchReturnAListOfDeliveriesMeetingRequiredCriteria()
         {
-            
+            var annexRecord = _fixture.Create<ResidentSupportAnnex>();
+            //annex record should not have a confirmed delivery
+            annexRecord.LastConfirmedFoodDelivery = null;
+            //annex record should have an ongoing food need of true
+            annexRecord.OngoingFoodNeed = true;
+            InsertIntoResidentSupportAnnexTable(annexRecord);
+            var response = _classUnderTest.CreateDeliverySchedule(10, "test");
+            response.Count.Should().Be(1);
+            response.First().AnnexId.Should().Be(annexRecord.Id);
         }
         
         private void ClearResidentSupportAnnexTable()
         {
             var addedEntities = _context.ResidentSupportAnnex;
             _context.ResidentSupportAnnex.RemoveRange(addedEntities);
+            _context.SaveChanges();
+        }
+
+        private void ClearDeliveryBatchTable()
+        {
+            var addedEntities = _context.DeliveryBatch;
+            _context.DeliveryBatch.RemoveRange(addedEntities);
+            _context.SaveChanges();
+        }
+
+        private void ClearDeliveryReportDataTable()
+        {
+            var addedEntities = _context.DeliveryReportData;
+            _context.DeliveryReportData.RemoveRange(addedEntities);
+            _context.SaveChanges();
+        }
+
+        private void InsertIntoResidentSupportAnnexTable(ResidentSupportAnnex request)
+        {
+            _context.ResidentSupportAnnex.Add(request);
             _context.SaveChanges();
         }
     }
