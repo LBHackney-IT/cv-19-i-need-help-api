4c7024647d57292e5c2c69284eb3776dc680d6f8
LBHSPreston <selwyn.preston@hackney.gov.uk>
2020-06-15 07:49:36 +0100

    New response object for forming annex summary data.

---
+++ cv-19-i-need-help/Boundary/V1/Responses/AnnexSummaryResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using CV19INeedHelp.Models.V1;
+
+namespace CV19INeedHelp.Boundary.V1.Responses
+{
+    public class AnnexSummaryResponse
+    {
+        public int ActiveCases { get; set; }
+    }
+}
