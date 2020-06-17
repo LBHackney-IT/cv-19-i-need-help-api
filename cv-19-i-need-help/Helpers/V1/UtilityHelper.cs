77314498ca7ee6f26b105c6f54ad6f506a9d733e
LBHSPreston <selwyn.preston@hackney.gov.uk>
2020-06-17 16:53:30 +0100

    Now need to make a database call to check for next bank holiday.

--- cv-19-i-need-help/Helpers/V1/UtilityHelper.cs
+++ cv-19-i-need-help/Helpers/V1/UtilityHelper.cs
@@ -1,17 +1,37 @@
 using System;
+using CV19INeedHelp.Gateways.V1;
+using CV19INeedHelp.Models.V1;
 
 namespace CV19INeedHelp.Helpers.V1
 {
     public class UtilityHelper : IUtilityHelper
     {
-        public DateTime GetNextWorkingDay()
+        private readonly IINeedHelpGateway _iHelperGateway;
+        public UtilityHelper(IINeedHelpGateway iHelperGateway)
         {
-            var nextDay = DateTime.Now.AddDays(1);
-            while (nextDay.DayOfWeek == DayOfWeek.Saturday || nextDay.DayOfWeek == DayOfWeek.Sunday)
+            _iHelperGateway = iHelperGateway;
+        }
+        public DateTime GetNextWorkingDay(DateTime date)
+        {
+            var nextDay = date.AddDays(1);
+            while (nextDay.DayOfWeek == DayOfWeek.Saturday || nextDay.DayOfWeek == DayOfWeek.Sunday || IsBankHoliday(nextDay))
             {
                 nextDay = nextDay.AddDays(1);
             }
             return nextDay;
+        }
+
+        private bool IsBankHoliday(DateTime date)
+        {
+            var nextBankHoliday = _iHelperGateway.GetNextBankHoliday(date);
+            if (nextBankHoliday == null)
+            {
+                return false;
+            }
+            else
+            {
+                return nextBankHoliday.Date == date;
+            }
         }
     }
 }
